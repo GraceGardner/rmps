d2380a00901cd028c4b4e94250c3cc742b9832d6
Grace Gardner <graceirisgardner@gmail.com>
2025-07-14 23:44:11 -0600

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,6 @@
-import React, { useState } from "react"
-
-import plt1 from "../assets/vendors/plt1.jpg"
+import React from "react"
 
 const Home: React.FC = () => {
-	const eventPhotos = [plt1, plt1, plt1, plt1, plt1, plt1, plt1]
-
-	const [currentIndex, setCurrentIndex] = useState(0)
-
-	const prevSlide = () => {
-		setCurrentIndex((prev) => (prev === 0 ? eventPhotos.length - 1 : prev - 1))
-	}
-
-	const nextSlide = () => {
-		setCurrentIndex((prev) => (prev === eventPhotos.length - 1 ? 0 : prev + 1))
-	}
-
 	return (
 		<div className="bg-stone-50 text-black min-h-screen pb-24 pt-32 px-4 sm:pt-96">
 			<div className="container mx-auto max-w-4xl text-center">
@@ -42,28 +28,8 @@ const Home: React.FC = () => {
 
 			<div className="container mx-auto text-center">
 				<h2 className="text-4xl font-bold text-black uppercase pt-10">
-					Photos from Last Year's Event
+					Photos from Last Year's Event???
 				</h2>
-
-				<div className="relative w-full max-w-2xl mx-auto shadow-lg rounded overflow-hidden">
-					<img
-						src={eventPhotos[currentIndex]}
-						alt={`Event Photo ${currentIndex + 1}`}
-						className="w-full h-96 object-cover"
-					/>
-					<button
-						onClick={prevSlide}
-						className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-3 py-2 hover:bg-opacity-75"
-					>
-						‹
-					</button>
-					<button
-						onClick={nextSlide}
-						className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-3 py-2 hover:bg-opacity-75"
-					>
-						›
-					</button>
-				</div>
 			</div>
 		</div>
 	)
